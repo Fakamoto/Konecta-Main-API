5620b8f1e91db326772dd4fa93bf9b9685add9b8
fgoiriz <fgoiriz@dyvenia.com>
2023-01-16 21:55:48 +0000

    Deleted base prompt, and updated da vinci to 003

--- api/services/openai.service.ts
+++ api/services/openai.service.ts
@@ -9,14 +9,13 @@ export class OpenaiService {
 
     async completion(prompt: string): Promise<{ text: string, tokens: number }> {
         const completion = await this.openai.createCompletion({
-            model: 'text-davinci-002',
-            prompt: `The following is a conversation with an Whatsapp chatbot powered by AI called "Konecta", it will reply in any language that the human is talking in. The assistant is helpful, creative, clever, and very friendly. Konecta will reply in spanish if the Human is talking in spanish\n\nHuman: ${prompt}.\nAI:`,
+            model: 'text-davinci-003',
+            prompt: prompt,
             temperature: 0.5,
             max_tokens: 500,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
-            stop:[' Human:', ' AI:']
         });
 
         console.log(completion.data.choices[0].text);
